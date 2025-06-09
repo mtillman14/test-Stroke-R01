fde259ab1ed9dcd73340d6a426d79f1e0080da08
[Mitchell Tillman] <[mtillman14@gmail.com]>
2025-06-08 19:06:04 -0500

    Renamed folder to package name

---
+++ debug.py
@@ -0,0 +1,4 @@
+import ross_cli
+import ross_cli.cli
+
+ross_cli.cli.install_command("load_gaitrite")
---
+++ src/test_Stroke_R01/paths.m
@@ -0,0 +1,5 @@
+% Specifies all pipeline configuration
+configPath = 'config.json';
+
+% Specifies which subjects to run
+subjectsToRunPath = 'subjects_to_run.toml';
